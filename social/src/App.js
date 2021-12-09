3d3abfe2a1b6005b3c0b0ad6d5c6fedc2b01905e
Abdullah-Aljumah <Abdullah.Ali.elq.js@tuwaiq.edu.sa>
2021-12-09 09:56:24 +0300

    build basic

---
+++ social/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Routes } from "react-router";
+import { Route } from "react-router";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/" element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
