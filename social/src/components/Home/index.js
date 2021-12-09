56a84b63581b1f1c14302f13667583c7d217c69d
Abdullah-Aljumah <Abdullah.Ali.elq.js@tuwaiq.edu.sa>
2021-12-09 14:50:56 +0300

    delete post

--- social/src/components/Home/index.js
+++ social/src/components/Home/index.js
@@ -4,26 +4,38 @@ import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
 const Home = () => {
-    
   const state = useSelector((state) => {
     return state;
   });
 
   const [posts, setPosts] = useState([]);
+
   const getPost = async () => {
-    const res = await axios.get(
-      `${process.env.REACT_APP_BASE_URL}/posts/${state.signIn.user._id}`,
-      {
-        headers: { Authorization: `Bearer ${state.signIn.token}` },
-      }
-    );
-    console.log(res.data);
+    const res = await axios.get(`${process.env.REACT_APP_BASE_URL}/allpost`);
     setPosts(res.data);
   };
+
   useEffect(() => {
     getPost();
     // eslint-disable-next-line
   }, []);
+
+  const logOut = () => {
+    localStorage.clear();
+  };
+
+  const deletePost = async (id) => {
+    console.log(id, "IDDD");
+
+    let res = await axios.delete(
+      `${process.env.REACT_APP_BASE_URL}/softDelete/${id}`,
+      {
+        headers: { Authorization: `Bearer ${state.signIn.token}` },
+      }
+    );
+    console.log(res, "RESRESRRES");
+    getPost();
+  };
 
   return (
     <div>
@@ -32,11 +44,29 @@ const Home = () => {
         posts.map((item, i) => {
           return (
             <div key={i}>
-              <img src={item.img} alt="post" />
-              <p>{item.desc}</p>
+              <p>@{item.user.username} </p>
+              <img src={item.img} alt="post" style={{ width: "20rem" }} />
+              <p>{item.desc} </p>
+              <p>{item.time} </p>
+              {state.signIn.user.role === "61a734cd947e8eba47efbc68" ||
+              state.signIn.user._id === item.user._id ? (
+                <button onClick={() => deletePost(item._id)}>
+                  Delete Post
+                </button>
+              ) : (
+                <p></p>
+              )}
+              <hr />
             </div>
           );
         })}
+
+      <form onClick={logOut}>
+        <button type="submit" style={{ cursor: "pointer" }}>
+          {" "}
+          Log out{" "}
+        </button>
+      </form>
     </div>
   );
 };
